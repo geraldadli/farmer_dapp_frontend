75833ec0ed5afe83facad8a9e3db57ef91f547f8
geraldadli <geraldadli4@gmail.com>
2025-04-19 16:12:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import SplashScreen    from './components/SplashScreen';
+import WelcomeScreen   from './components/WelcomeScreen';
+import InfoScreen      from './components/InfoScreen';
+import RoleSelection   from './components/RoleSelection';
+import Register        from './components/Register';
+import Login           from './components/Login';
+import Home            from './components/Home';
+import Cash          from './components/Cash';
+import Notification  from './components/Notification';
+import Profile       from './components/Profile';
+import Settings     from './components/Settings';
+
+import './index.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/"               element={<SplashScreen />} />
+        <Route path="/welcome"        element={<WelcomeScreen />} />
+        <Route path="/info"           element={<InfoScreen />} />
+        <Route path="/roles"          element={<RoleSelection />} />
+        <Route path="/:role/register" element={<Register />} />
+        <Route path="/:role/login"    element={<Login />} />
+        <Route path="/home"           element={<Home />} />
+        <Route path="/cash"           element={<Cash />} />
+        <Route path="/mail"           element={<Notification />} />
+        <Route path="/profile"        element={<Profile />} />
+        <Route path="/settings"       element={<Settings />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
